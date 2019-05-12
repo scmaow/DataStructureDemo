78e4dbf1acf0df30224db02c869ca5946b94c734
zhexue13 <scthiteen@163.com>
2019-05-12 17:14:36 +0800

    task#005

--- LinearList/SinglyLinkedList.cpp
+++ LinearList/SinglyLinkedList.cpp
@@ -1,8 +1,19 @@
 #include "SinglyLinkedList.h"
 #include <cstdlib>
 
+int init_slink(LinkList *list) {
+	*list = (Node *)malloc(sizeof(Node));
+	if (!(*list)) return -1;
+
+	(*list)->data = 0;
+	(*list)->next = NULL;
+
+	return 0;
+}
+
+
 int get_ele_slink(LinkList list, int pos, int *e) {
-	LinkList p = list->next;
+	Node *p = list->next;
 	int j = 1;
 
 	while (p && j < pos) {
@@ -17,11 +28,32 @@ int get_ele_slink(LinkList list, int pos, int *e) {
 	return 0;
 }
 
-int insert_ele_slink(LinkList list, int pos, int e) {
-	LinkList p = list;
+int length_slink(LinkList list) {
+	Node *p = list->next;
 	int j = 0;
 
-	while (p&&j < pos - 1) {
+	while (!p) {
+		p = p->next;
+		j++;
+	}
+	return j;
+}
+
+void print_slink(LinkList list) {
+	Node *p = list->next;
+	while (p) {
+		printf("%-5d", p->data);
+		p = p->next;
+	}
+	printf("\n");
+}
+
+int insert_ele_slink(LinkList list, int pos, int e) {
+
+	Node *p = list;
+	int j = 0;
+
+	while (p && j < pos - 1) {  //找到直接前驱元素
 		p = p->next;
 		j++;
 	}
@@ -31,8 +63,66 @@ int insert_ele_slink(LinkList list, int pos, int e) {
 	Node *node = (Node *)malloc(sizeof(Node));
 	node->data = e;
 	node->next = p->next;
-
 	p->next = node;
 
+	return 0;
+}
+
+int delete_ele_slink(LinkList list, int pos, int *e) {
+	Node *p = list;
+	int j = 0;
+
+	while (p->next && j < pos - 1) {  //找到直接前驱元素
+		p = p->next;
+		j++;
+	}
+
+	if (!(p->next) || j > pos - 1) return -1;
+
+	Node *node = p->next;
+	p->next = node->next;
+	*e = node->data;
+	free(node);
+
+	return 0;
+}
+
+int create_reverse_slink(LinkList *list, int n) {
+	if (init_slink(list)) return -1;
+
+	FILE *pf = fopen("SLinkListTestData.txt", "r");
+
+	int tmp;
+	for (int i = n; i > 0; i--) {
+		if (fscanf(pf, "%d", &tmp) == 1) {
+			Node *node = (Node *)malloc(sizeof(Node));
+			node->data = tmp;
+			node->next = (*list)->next;
+			(*list)->next = node;
+		}
+	}
+	fclose(pf);
+	return 0;
+}
+
+int create_positive_slink(LinkList *list, int n) {
+	if (init_slink(list)) return -1;
+
+	Node *p = *list;
+
+	FILE *pf = fopen("SLinkListTestData.txt", "r");
+
+	int tmp;
+	for (int i = n; i > 0; i--) {
+		if (fscanf(pf, "%d", &tmp) == 1) {
+			Node *node = (Node *)malloc(sizeof(Node));
+			node->data = tmp;
+			node->next = NULL;
+
+			p->next = node;
+			p = p->next;
+		}
+	}
+	fclose(pf);
 	return 0;
 }
--- LinearList/SinglyLinkedList.h
+++ LinearList/SinglyLinkedList.h
@@ -1,5 +1,5 @@
 /**
-* 线性表的链式实现 单链表 Singly Linked List
+* 线性表的链式实现 -- 单链表(Singly Linked List)
 *
 * 05/12/2019
 */
@@ -9,6 +9,7 @@
 
 #include <cstdio>
 
+//单链表的存储结构
 struct Node {
 	int data;
 	struct Node *next;
@@ -16,9 +17,67 @@ struct Node {
 typedef struct Node Node;
 typedef struct Node* LinkList;
 
+                  /************************************
+                    以下个函数处理的单链表均带有头结点
+                  ************************************/
+
+/**
+ * 初始化单链表
+ *
+ * 
+*/
+int init_slink(LinkList *list);
+				  
+				  
+/**
+ * 获取元素
+ * 
+ * pos >= 1
+*/
 int get_ele_slink(LinkList list,int pos,int *e);
 
+/**
+ * 获取单链表长度
+ *
+ * 
+*/
+int length_slink(LinkList list);
+
+/**
+ * 打印链表
+ *
+ * 
+*/
+void print_slink(LinkList list);
+
+/**
+ * 第pos个位置之前插入元素e
+ *
+ * pos >= 1
+*/
 int insert_ele_slink(LinkList list, int pos, int e);
+
+/**
+ * 第pos个位置的元素删除
+ *
+ * pos >= 1
+*/
+int delete_ele_slink(LinkList list, int pos, int *e);
+
+/**
+ * 从表尾到表头逆向建立单链表
+ *
+ * n >= 0
+*/
+int create_reverse_slink(LinkList *list, int n);
+
+
+/**
+* 建立正向单链表
+*
+* n >= 0
+*/
+int create_positive_slink(LinkList *list, int n);
 
 #endif // !SINGLY_LINKED_LIST_H
 
